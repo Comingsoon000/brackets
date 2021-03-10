8a8551705771c9bb83f30b21245ac16f11a25937
Comingsoon <comingsoon000@ya.ru>
2021-03-10 13:15:40 +0300

    init: start Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  for (const item of str) {
+    for (const brackets of bracketsConfig) {
+      if (item === brackets[0]) {
+        if (brackets[0] === brackets[1]) {
+          if (stack[stack.length - 1] === item) {
+            stack.pop();
+            continue;
+          }
+        }
+        stack.push(brackets[1]);
+        continue;
+      }
+      if (item === brackets[1]) {
+        if (stack.pop() !== item) {
+          return false;
+        }
+      }
+    }
+  }
+  return (stack.length === 0);
 }
